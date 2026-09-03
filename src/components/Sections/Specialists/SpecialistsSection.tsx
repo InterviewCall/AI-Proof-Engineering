468a3d5b86962476375c87deb95007b3a9543836
Madhuboni Bhattacharya <madhubonibhattacharya03@gmail.com>
2026-09-03 19:11:07 +0530

    Specialists Section Width Fixed

--- src/components/Sections/Specialists/SpecialistsSection.tsx
+++ src/components/Sections/Specialists/SpecialistsSection.tsx
@@ -25,7 +25,7 @@ const SpecialistsSection: FC = () => {
                 xl:px-12
             "
         >
-            <div className="mx-auto w-full max-w-345">
+            <div className="mx-auto w-full max-w-1540">
                 {/* Header */}
                 <header className="mx-auto max-w-280 text-center">
                     <div
